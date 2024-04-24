bef39e040042b6a6035ac466861efdacc5ed30d6
sujith surendran <sujithsurendran4433@gmail.com>
2024-04-24 17:02:44 +0530

    homepage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Homepage/Hero';
+import Footer from './components/Footer/Footer';
+import WhyUs from './components/Homepage/WhyUs';
+import About from './components/Homepage/About';
+import Mentors from './components/Homepage/Mentors';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Mentors />
+      <About />
+      <WhyUs />
+      <Footer />
+    </>
   );
 }
 
